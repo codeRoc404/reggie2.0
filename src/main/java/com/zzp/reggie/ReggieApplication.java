718b66790699e382ff1c939ed5ae19da1f130395
roc <2460718527@qq.com>
2023-02-21 18:25:54 +0800

    reggie2.0 test1

--- src/main/java/com/zzp/reggie/ReggieApplication.java
+++ src/main/java/com/zzp/reggie/ReggieApplication.java
@@ -38,5 +38,6 @@ public class ReggieApplication {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
     }
 }
