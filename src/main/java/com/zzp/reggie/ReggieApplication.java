d6aa08186a2fd3db1481d0c19d56df08c2f40ae0
404 <66376442+codeRoc404@users.noreply.github.com>
2023-02-21 18:28:40 +0800

    Update ReggieApplication.java

--- src/main/java/com/zzp/reggie/ReggieApplication.java
+++ src/main/java/com/zzp/reggie/ReggieApplication.java
@@ -39,5 +39,6 @@ public class ReggieApplication {
         System.out.println("hello2");
         System.out.println("hello3");
         System.out.println("hello4");
+        System.out.println("hello5");
     }
 }
